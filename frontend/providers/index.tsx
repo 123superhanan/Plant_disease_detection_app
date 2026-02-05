2c83122c6e9c43214ab1f28a6fba0f11711648ad
Abdul Hanan Abbasi <146124897+123superhanan@users.noreply.github.com>
2026-02-05 18:58:39 +0500

    Track frontend as normal directory

---
+++ frontend/providers/index.tsx
@@ -0,0 +1,13 @@
+// import React  from 'react'
+// import { Text, View } from 'react-native'
+
+// const AppProviders =({ children }:{ children: React.ReactNode }) => {
+
+//     return (
+//       <ClerkProvider>
+//         {children}
+//       </ClerkProvider>
+//     )
+  
+// }
+// export default AppProviders
